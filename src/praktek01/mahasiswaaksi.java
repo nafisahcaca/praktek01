4beb36370ee78a810201babac5f9661ebb8cdc7e
nafisahcaca <imaryani397@gmail.com>
2018-03-28 23:45:06 +0800

    mengisis nilai atribut

--- src/praktek01/mahasiswaaksi.java
+++ src/praktek01/mahasiswaaksi.java
@@ -12,5 +12,11 @@ package praktek01;
 public class mahasiswaaksi {
     public static void main(String[] args) {
         mahasiswa caca = new mahasiswa();
+        
+        caca.nama="mahfudhotin nafisahtul";
+        caca.NPM="16630266";
+        caca.jurusan="FTI";
+        caca.kelas="4F";
+        caca.IPK=3.4;
     }
 }
